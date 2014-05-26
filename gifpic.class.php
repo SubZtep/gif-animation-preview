232438b001d1ad8761604be0b9d8dac31610c4e0
Andras Serfozo <subztep@gmail.com>
2014-05-26 19:26:55 +0100

    Reformat for Wordpress style

--- gifpic.class.php
+++ gifpic.class.php
@@ -1,22 +1,22 @@
 <?php
-class GifPic {
+class Gif_Pic {
 
     public $preview_suffix = '-gifpreview'; // Preview image filename suffix
     public $watermark;
     public $filename;
 
-    public function __construct($filename = null) {
-        $this->watermark = __DIR__.'/play.png';
-        if (! is_null($filename)) {
+    public function __construct( $filename = null ) {
+        $this->watermark = __DIR__ . '/play.png';
+        if ( ! is_null( $filename ) ) {
             $this->filename = $filename;
         }
     }
 
     public function getPreview() {
-        $img_path = $this->getPathFromUrl($this->filename);
-        $preview_filename = pathinfo($img_path, PATHINFO_FILENAME) . $this->preview_suffix . '.jpg';
-        if (file_exists(dirname($img_path) .'/'. $preview_filename)) {
-            return pathinfo($this->filename, PATHINFO_DIRNAME) .'/'. $preview_filename;
+        $img_path = $this->getPathFromUrl( $this->filename );
+        $preview_filename = pathinfo( $img_path, PATHINFO_FILENAME ) . $this->preview_suffix . '.jpg';
+        if ( file_exists( dirname( $img_path ) . '/' . $preview_filename ) ) {
+            return pathinfo( $this->filename, PATHINFO_DIRNAME ) . '/' . $preview_filename;
         } else {
             return $this->generatePreview();
         }
@@ -44,50 +44,51 @@ class GifPic {
     }
 
     public function generatePreview() {
-        $img_path = $this->getPathFromUrl($this->filename);
+        $img_path = $this->getPathFromUrl( $this->filename );
 
-        $image = @imagecreatefromgif($img_path);
-        if ($image === false) {
+        $image = @imagecreatefromgif( $img_path );
+        if ( $image === false ) {
             return false;
         }
 
-        $water_size = getimagesize($this->watermark);
-        switch ($water_size['mime']) {
-            case 'image/png': $watermark = @imagecreatefrompng($this->watermark); break;
-            case 'image/gif': $watermark = @imagecreatefromgif($this->watermark); break;
-            case 'image/jpg': 
-            case 'image/jpeg': $watermark = @imagecreatefromjpeg($this->watermark); break;
+        $water_size = getimagesize( $this->watermark );
+        switch ( $water_size['mime'] ) {
+            case 'image/png': $watermark = @imagecreatefrompng( $this->watermark ); break;
+            case 'image/gif': $watermark = @imagecreatefromgif( $this->watermark ); break;
+            case 'image/jpg':
+            case 'image/jpeg': $watermark = @imagecreatefromjpeg( $this->watermark ); break;
             default: return false;
         }
 
-        $w = imagesx($image);
-        $h = imagesy($image);
-        $ww = imagesx($watermark);
-        $wh = imagesy($watermark);
-        $cut = imagecreatetruecolor($w, $h);
-        imagecopy($cut, $image, 0, 0, 0, 0, $w, $h);
-        imagecopy($cut, $watermark, (($w/2)-($ww/2)), (($h/2)-($wh/2)), 0, 0, $ww, $wh);
+        $w = imagesx( $image );
+        $h = imagesy( $image );
+        $ww = imagesx( $watermark );
+        $wh = imagesy( $watermark );
+        $cut = imagecreatetruecolor( $w, $h );
+        imagecopy( $cut, $image, 0, 0, 0, 0, $w, $h );
+        imagecopy( $cut, $watermark, ( ( $w / 2 ) - ( $ww / 2 ) ), ( ( $h / 2 ) - ( $wh / 2 ) ), 0, 0, $ww, $wh );
 
-        $preview_filename = pathinfo($img_path, PATHINFO_FILENAME) . $this->preview_suffix . '.jpg';
-        $res = imagepng($cut, dirname($img_path) .'/'. $preview_filename);
-        imagedestroy($image);
-        imagedestroy($watermark);
-        imagedestroy($cut);
+        $preview_filename = pathinfo( $img_path, PATHINFO_FILENAME ) . $this->preview_suffix . '.jpg';
+        $res = imagepng( $cut, dirname( $img_path ) . '/' . $preview_filename );
+        imagedestroy( $image );
+        imagedestroy( $watermark );
+        imagedestroy( $cut );
         if (! $res) {
             return false;
         }
-        return pathinfo($this->filename, PATHINFO_DIRNAME) .'/'. $preview_filename;
+        return pathinfo( $this->filename, PATHINFO_DIRNAME ) . '/' . $preview_filename;
     }
 
-    private function getPathFromUrl($url) {
+    private function getPathFromUrl( $url ) {
         $site_url = get_site_url();
-        if (strpos($this->filename, $site_url) === 0) {
-            $url = substr($url, strlen($site_url));
-            if (substr($url, 0, 1) != '/') {
-                $url = '/'.$url;
+        if ( strpos( $this->filename, $site_url ) === 0 ) {
+            $url = substr( $url, strlen( $site_url ) );
+            if ( substr( $url, 0, 1 ) != '/' ) {
+                $url = '/' . $url;
             }
-            return realpath(ABSPATH) . $url;
+            return realpath( ABSPATH ) . $url;
         }
         return false;
     }
 }
+?>
--- plugin.php
+++ plugin.php
@@ -7,74 +7,10 @@ Author: Andras Serfozo
 Author URI: http://twitter.com/SubZtep
 License: GPLv2 or later
 */
-require_once(__DIR__.'/gifpic.class.php');
 
-class GifAnimationPreview {
-
-    protected $gifpic;
-
-    private static $plugin;
-    static function load() {
-        $class = __CLASS__; 
-        return ( self::$plugin ? self::$plugin : ( self::$plugin = new $class() ) );
-    }
-
-    private function __construct() {
-        add_action('the_content', array($this, 'replace_gifs'));
-        $this->gifpic = new GifPic();
-    }
-
-    public function replace_gifs($content) {
-        // Find all img tags in the post
-        return preg_replace_callback('/<img[^>]+>/i', function($img_tag) {
-            $original_src = false; // Original img src
-            $new_src = false; // Gif anim preview src
-
-            // Get src property
-            $patterns = array('/(src=")([^"]+)(")/i', '/(src=\')([^\']+)(\')/i');
-            $new_img_tag = preg_replace_callback($patterns, function($attr) use (&$original_src, &$new_src) {
-                // Test only gif
-                if (substr(strtolower($attr[2]), -4) == '.gif') {
-                    $this->gifpic->filename = $attr[2];
-                    if ($this->gifpic->isAnimation()) {
-                        // Get preview link
-                        $original_src = $attr[2];
-                        $new_src = $this->gifpic->getPreview();
-                        return $attr[1].$new_src.$attr[3];
-                    }
-                }
-                // Not gif nor animation, do nothing
-                return $attr[0];
-            }, $img_tag[0]);
-
-            // Apply modifiers if necessary
-            if ($new_src !== false) {
-                // overwrite onclick attribute
-                $onclick = "if(this.src.indexOf('".$this->gifpic->preview_suffix."')!=-1){this.src='$original_src';}else{this.src='$new_src';}return false;";
-                $found = false;
-                $patterns = array('/(onclick=")([^"]+)(")/i', '/(onclick=\')([^\']+)(\')/i');
-                $img_tag = preg_replace_callback($patterns, function($attr) use ($onclick, &$found) {
-                    $found = true;
-                    return $attr[1].$attr[2].$onclick.$attr[3];
-                }, $new_img_tag);
-
-                if (! $found) {
-                    // no onclick attribute, simply insert onclick before src 
-                    $patterns = array('/( src="[^"]+")/i', '/( src=\'[^\']+\')/i');
-                    $img_tag = preg_replace_callback($patterns, function($attr) use ($onclick) {
-                        return $attr[0].' onclick="'.$onclick.'"';
-                    }, $new_img_tag);
-                }
-            }
-
-            if (is_array($img_tag)) {
-                return $img_tag[0];
-            }
-            return $img_tag;
-
-        }, $content);
-    }
-
+if (! is_admin()) {
+    require_once( __DIR__ . '/gap.class.php' );
+    GIF_Animation_Preview::load();
 }
 
-GifAnimationPreview::load();
+?>
