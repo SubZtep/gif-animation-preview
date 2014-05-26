a3e4b958c39043c75ce885457cc591dca596f6a4
Andras Serfozo <subztep@gmail.com>
2014-05-26 19:30:48 +0100

    Add tests during install

--- plugin.php
+++ plugin.php
@@ -8,6 +8,25 @@ Author URI: http://twitter.com/SubZtep
 License: GPLv2 or later
 */
 
+register_activation_hook(__FILE__, 'gap_test_env');
+
+function gap_test_env() {
+    $errors = array();
+    if (version_compare(PHP_VERSION, '5.3.0', '<')) {
+        $errors[] = 'This plugin requires at least PHP version 5.3';
+    }
+    if (! extension_loaded('gd') || ! function_exists('gd_info')) {
+        $errors[] = 'This plugin requires GD support';
+    }
+    if (! empty($errors)) {
+        deactivate_plugins( basename( __FILE__ ) );
+        wp_die(implode('<br />', $errors) . '<br /><br />Please contact with your hosting provider',
+               'Plugin Activation Error',
+               array( 'response' => 200, 'back_link' => true ) );
+    }
+    return true;
+}
+
 if (! is_admin()) {
     require_once( __DIR__ . '/gap.class.php' );
     GIF_Animation_Preview::load();
