6ed2837e54ead69110728dfb7a78d6203e9d489e
Andras Serfozo <subztep@gmail.com>
2014-06-02 23:31:44 +0100

    Merge plugin classes

--- gap.class.php
+++ gap.class.php
@@ -3,6 +3,7 @@ require_once( __DIR__ . '/gifpic.class.php' );
 
 class GIF_Animation_Preview {
 
+    public $preview_suffix = '-gap'; // Preview image filename suffix
     protected $gifpic;
 
     private static $plugin;
@@ -38,17 +39,69 @@ class GIF_Animation_Preview {
     function update_src( $src ) {
         // Test only gif
         if ( substr( strtolower( $src[2] ), -4) == '.gif' ) {
-            $this->gifpic->filename = $src[2];
-            if ( $this->gifpic->isAnimation() ) {
+            if ( $this->is_animation( $src[2] ) ) {
                 // Get preview link
-                $new_src = $this->gifpic->getPreview();
-                if ($new_src) {
+                $new_src = $this->get_preview( $src[2] );
+                if ( $new_src ) {
                     return $src[1] . $new_src . $src[3] . ' data-gif=' . $src[3] . $src[2] . $src[3];
                 }
             }
         }
         // Not gif nor animation, do nothing
         return $src[0];
+    }
+
+    public function is_animation( $filename ) {
+        if ( ! ( $fh = @fopen( $filename, 'rb' ) ) ) {
+            return false;
+        }
+        $count = 0;
+        while ( ! feof($fh) && $count < 2 ) {
+            $chunk = fread( $fh, 1024 * 100 );
+            $count += preg_match_all( '#\x00\x21\xF9\x04.{4}\x00\x2C#s', $chunk, $matches );
+        }
+        fclose( $fh );
+        return $count > 1;
+    }
+
+    public function get_preview( $filename ) {
+        $img_path = $this->get_path_from_url( $filename );
+        $preview_filename = pathinfo( $img_path, PATHINFO_FILENAME ) . $this->preview_suffix . '.jpg';
+        if ( file_exists( dirname( $img_path ) . '/' . $preview_filename ) ) {
+            return pathinfo( $filename, PATHINFO_DIRNAME ) . '/' . $preview_filename;
+        } else {
+            return $this->generate_preview( $filename );
+        }
+    }
+
+    public function generate_preview( $filename ) {
+        $img_path = $this->get_path_from_url( $filename );
+        $preview_file = pathinfo( $img_path, PATHINFO_FILENAME ) . $this->preview_suffix . '.jpg';
+        $image = @imagecreatefromgif( $img_path );
+        if ( $image === false ) {
+            return false;
+        }
+        $w = imagesx( $image );
+        $h = imagesy( $image );
+        $cut = imagecreatetruecolor( $w, $h );
+        imagecopy( $cut, $image, 0, 0, 0, 0, $w, $h );
+        $res = imagejpeg( $cut, dirname( $img_path ) . '/' . $preview_file, 80 );
+        if (! $res) {
+            return false;
+        }
+        return pathinfo( $filename, PATHINFO_DIRNAME ) . '/' . $preview_file;
+    }
+
+    private function get_path_from_url( $filename ) {
+        $site_url = get_site_url();
+        if ( strpos( $filename, $site_url ) === 0 ) {
+            $url = substr( $filename, strlen( $site_url ) );
+            if ( substr( $filename, 0, 1 ) != '/' ) {
+                $url = '/' . $url;
+            }
+            return realpath( ABSPATH ) . $url;
+        }
+        return false;
     }
 }
 ?>
