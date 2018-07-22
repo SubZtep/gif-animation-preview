a032d261bbc0b2a7fe3ee56b39dc92a1658726a7
Andras Serfozo <subztep@gmail.com>
2018-07-22 12:44:02 +0100

    JS WP Coding Standards

--- src/gapplayer.js
+++ src/gapplayer.js
@@ -5,11 +5,11 @@
  * Released under the MIT license
  */
 
-(function($) {
+(function( $ ) {
 
-	function GapPlayer(preview, options) {
+	function GapPlayer( preview, options ) {
 		this.previewElement = preview;
-		this.spinnerElement = $("<div class='spinner'></div>");
+		this.spinnerElement = $( '<div class=\'spinner\'></div>' );
 		this.options = options;
 		this.gifLoaded = false;
 	}
@@ -21,52 +21,52 @@
 			this.addSpinner();
 			this.addControl();
 			this.addEvents();
-			if (this.options.autoLoad) {
+			if ( this.options.autoLoad ) {
 				this.playElement.hide();
 				this.spinnerElement.show();
 				this.loadGif();
 			}
-			if (this.options.preLoad) {
-				(new Image()).src = this.getGifSrc();
+			if ( this.options.preLoad ) {
+				( new Image() ).src = this.getGifSrc();
 			}
 		},
 
 		wrap: function() {
-			this.wrapper = this.previewElement.wrap("<div class='gapplayer-wrapper'></div>").parent();
-			this.wrapper.css('width', this.previewElement.width());
-			this.wrapper.css('height', this.previewElement.height());
-			this.previewElement.addClass('gapplayer');
-			this.previewElement.css('cursor','pointer');
+			this.wrapper = this.previewElement.wrap( '<div class=\'gapplayer-wrapper\'></div>' ).parent();
+			this.wrapper.css( 'width', this.previewElement.width() );
+			this.wrapper.css( 'height', this.previewElement.height() );
+			this.previewElement.addClass( 'gapplayer' );
+			this.previewElement.css( 'cursor', 'pointer' );
 		},
 
 		getGifSrc: function() {
 			var gifSrc;
-			if (this.previewElement.attr('data-gif')) {
-				gifSrc = this.previewElement.attr('data-gif');
+			if ( this.previewElement.attr( 'data-gif' ) ) {
+				gifSrc = this.previewElement.attr( 'data-gif' );
 			} else {
-				gifSrc = this.previewElement.attr('src').replace(/\.[^/.]+$/, ".gif");
+				gifSrc = this.previewElement.attr( 'src' ).replace( /\.[^/.]+$/, '.gif' );
 			}
 			return gifSrc;
 		},
 
 		addControl: function() {
-			this.playElement = $("<ins class='play-gif'>" + this.options.label + "</ins>");
-			this.playElement.css('left', this.previewElement.width()/2 + this.playElement.width()/2);
-			this.wrapper.append(this.playElement);
+			this.playElement = $( '<ins class=\'play-gif\'>' + this.options.label + '</ins>' );
+			this.playElement.css( 'left', this.previewElement.width() / 2 + this.playElement.width() / 2 );
+			this.wrapper.append( this.playElement );
 		},
 
 		addEvents: function() {
 			var onEvent = this.options.hover ? 'mouseenter' : 'click',
 				gp = this;
-			gp.playElement.on(onEvent, function(e) {
-				$(this).hide();
+			gp.playElement.on( onEvent, function( e ) {
+				$( this ).hide();
 				gp.spinnerElement.show();
 				gp.loadGif();
 				e.preventDefault();
 				e.stopPropagation();
 			});
-			gp.previewElement.on(onEvent, function(e) {
-				if (gp.playElement.is(':visible')) {
+			gp.previewElement.on( onEvent, function ( e ) {
+				if ( gp.playElement.is( ':visible' ) ) {
 					gp.playElement.hide();
 					gp.spinnerElement.show();
 					gp.loadGif();
@@ -74,14 +74,14 @@
 				e.preventDefault();
 				e.stopPropagation();
 			});
-			gp.spinnerElement.on(onEvent, function(e) {
+			gp.spinnerElement.on( onEvent, function( e ) {
 				e.preventDefault();
 				e.stopPropagation();
 			});
 		},
 
 		loadGif: function() {
-			if (! this.gifLoaded) {
+			if ( ! this.gifLoaded ) {
 				this.enableAbort();
 			}
 			var gp = this,
@@ -90,38 +90,40 @@
 				gifWidth = this.previewElement.width(),
 				gifHeight = this.previewElement.height();
 
-			gp.gifElement = $("<img src='" + gifSrc + "' width='" + gifWidth + "' height=' "+ gifHeight + " '/>");
-			this.gifElement.load(function() {
+			gp.gifElement = $( '<img src="' + gifSrc + '" width="' + gifWidth + '" height="' + gifHeight + '" />' );
+			this.gifElement.load( function() {
 				gp.gifLoaded = true;
 				gp.resetEvents();
-				$(this).css({'position': 'absolute',
-							'top': '0',
-							'left': '0'});
+				$( this ).css( {
+					'position': 'absolute',
+					'top': '0',
+					'left': '0'
+				} );
 
 				// Start animation
-				if (gp.options.effect) {
+				if ( gp.options.effect ) {
 					gp.gifElement.hide();
 					gp.spinnerElement.hide();
-					gp.wrapper.append(gp.gifElement);
-					gp.gifElement.stop(true).fadeIn(function() {
+					gp.wrapper.append( gp.gifElement );
+					gp.gifElement.stop( true ).fadeIn( function() {
 						gp.previewElement.hide();
 					});
 				} else {
 					gp.previewElement.hide();
-					gp.wrapper.append(gp.gifElement);
+					gp.wrapper.append( gp.gifElement );
 					gp.spinnerElement.hide();
 				}
 
-				if (! gp.options.autoLoad) {
-					$(this).on(onEvent, function(e) {
+				if ( ! gp.options.autoLoad ) {
+					$( this ).on( onEvent, function( e ) {
 
 						// Stop animation
-						if (gp.options.effect) {
+						if ( gp.options.effect ) {
 							gp.previewElement.show();
 							gp.playElement.show();
-							$(this).stop(true).fadeOut();
+							$( this ).stop( true ).fadeOut();
 						} else {
-							$(this).remove();
+							$( this ).remove();
 							gp.previewElement.show();
 							gp.playElement.show();
 						}
@@ -135,20 +137,20 @@
 
 		enableAbort: function() {
 			var gp = this;
-			this.previewElement.click(function(e) {
-				gp.abortLoading(e);
+			this.previewElement.click( function( e ) {
+				gp.abortLoading( e );
 			});
-			this.spinnerElement.click(function(e) {
-				gp.abortLoading(e);
+			this.spinnerElement.click( function( e ) {
+				gp.abortLoading( e );
 			});
 		},
 
-		abortLoading: function(e) {
+		abortLoading: function( e ) {
 			this.spinnerElement.hide();
 			this.playElement.show();
 			e.preventDefault();
 			e.stopPropagation();
-			this.gifElement.off('load').on('load', function(ev) {
+			this.gifElement.off( 'load' ).on( 'load', function( ev ) {
 				ev.preventDefault();
 				ev.stopPropagation();
 			});
@@ -156,23 +158,23 @@
 		},
 
 		resetEvents: function() {
-			this.previewElement.off('click');
-			this.playElement.off('click');
-			this.spinnerElement.off('click');
+			this.previewElement.off( 'click' );
+			this.playElement.off( 'click' );
+			this.spinnerElement.off( 'click' );
 			this.addEvents();
 		},
 
 		addSpinner: function() {
-			this.wrapper.append(this.spinnerElement);
+			this.wrapper.append( this.spinnerElement );
 			this.spinnerElement.hide();
 		}
 
 	};
 
-	$.fn.gapPlayer = function(options) {
-		return this.each(function() {
-			options = $.extend({}, $.fn.gapPlayer.defaults, options);
-			var gapPlayer = new GapPlayer($(this), options);
+	$.fn.gapPlayer = function( options ) {
+		return this.each( function() {
+			options = $.extend( {}, $.fn.gapPlayer.defaults, options );
+			var gapPlayer = new GapPlayer( $( this ), options );
 			gapPlayer.activate();
 		});
 	};
@@ -188,42 +190,42 @@
 	// Start plugin
 	gapStart();
 
-})(jQuery);
+})( jQuery );
 
 function gapStart() {
-	if (gapParams.metadata === 'yes') {
-		jQuery('img[src$="-gap.jpg"]:not([data-gif]):not(.gapplayer),img[src*="-gap.jpg?"]:not([data-gif]):not(.gapplayer)').each(function() {
-			var src = jQuery(this).attr('src');
-			jQuery(this).attr('data-gif', src.substring(0, src.length - 8) + '.gif');
+	if ( gapParams.metadata === 'yes' ) {
+		jQuery( 'img[src$="-gap.jpg"]:not([data-gif]):not(.gapplayer),img[src*="-gap.jpg?"]:not([data-gif]):not(.gapplayer)' ).each( function() {
+			var src = jQuery( this ).attr( 'src' );
+			jQuery( this ).attr( 'data-gif', src.substring( 0, src.length - 8 ) + '.gif' );
 		});
 	}
 
 	var gapPlayerParams = {
-		autoLoad: gapParams.autoLoad == 'yes',
-		preLoad: gapParams.preLoad == 'no',
-		effect: gapParams.effect == 'yes',
-		hover: gapParams.hover == 'yes'
+		autoLoad: gapParams.autoLoad === 'yes',
+		preLoad: gapParams.preLoad === 'no',
+		effect: gapParams.effect === 'yes',
+		hover: gapParams.hover === 'yes'
 	};
 
 	var noLazyGifsArr = [];
-	jQuery('img[data-gif]').each(function() {
-		var img = jQuery(this);
+	jQuery( 'img[data-gif]' ).each( function() {
+		var img = jQuery( this );
 
-		if (img.hasClass('lazy-hidden')) {
+		if ( img.hasClass( 'lazy-hidden' ) ) {
 			// Handle BJ Lazy Load
-			img.on('lazyloaded', function() {
-				img.imagesLoaded(function() {
-					img.gapPlayer(gapPlayerParams);
+			img.on( 'lazyloaded', function() {
+				img.imagesLoaded( function() {
+					img.gapPlayer( gapPlayerParams );
 				});
-				img.off('lazyloaded');
+				img.off( 'lazyloaded' );
 			})
 		} else {
 			// Not lazy loaded
-			noLazyGifsArr.push(img);
+			noLazyGifsArr.push( img );
 		}
 	});
-	var noLazyGifs = jQuery(noLazyGifsArr);
-	noLazyGifs.imagesLoaded(function() {
-		noLazyGifs.gapPlayer(gapPlayerParams);
+	var noLazyGifs = jQuery( noLazyGifsArr );
+	noLazyGifs.imagesLoaded( function() {
+		noLazyGifs.gapPlayer( gapPlayerParams );
 	});
 }
